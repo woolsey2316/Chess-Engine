8b88379e7ccb481eafd11ea2737db3387f9f0b35
David W <woolsey2316@gmail.com>
2024-08-01 16:40:36 +1000

    slider piece attacks on the fly

--- bbc.c
+++ bbc.c
@@ -201,10 +201,19 @@ U64 mask_bishop_attacks(int square)
   int tr = square / 8;
   int tf = square % 8;
 
-  for (r = tr + 1, f = tf + 1; r <= 6 && f <= 6; r++, f++) attacks |= (1ULL << (r * 8 + f));
-  for (r = tr - 1, f = tf + 1; r >= 1 && f <= 6; r--, f++) attacks |= (1ULL << (r * 8 + f));
-  for (r = tr + 1, f = tf - 1; r <= 6 && f >= 1; r++, f--) attacks |= (1ULL << (r * 8 + f));
-  for (r = tr - 1, f = tf - 1; r >= 1 && f >= 1; r--, f--) attacks |= (1ULL << (r * 8 + f));
+  for (r = tr + 1, f = tf + 1; r <= 6 && f <= 6; r++, f++) {
+    attacks |= (1ULL << (r * 8 + f));
+  }
+  for (r = tr - 1, f = tf + 1; r >= 1 && f <= 6; r--, f++){
+    attacks |= (1ULL << (r * 8 + f));
+  }
+  for (r = tr + 1, f = tf - 1; r <= 6 && f >= 1; r++, f--){
+    attacks |= (1ULL << (r * 8 + f));
+  }
+    attacks |= (1ULL << (r * 8 + f));
+  for (r = tr - 1, f = tf - 1; r >= 1 && f >= 1; r--, f--){
+    attacks |= (1ULL << (r * 8 + f));
+  }
   
   return attacks;
 
@@ -229,6 +238,69 @@ U64 mask_rook_attacks(int square)
 
 }
 
+U64 bishop_attacks_on_the_fly(int square, U64 block)
+{
+  // result attacks bitboard
+  U64 attacks = 0ULL;
+  
+  int r, f;
+
+  int tr = square / 8;
+  int tf = square % 8;
+
+  for (r = tr + 1, f = tf + 1; r <= 7 && f <= 7; r++, f++) {
+    attacks |= (1ULL << (r * 8 + f));
+    if (1ULL << (r * 8 + f) & block) break;
+  }
+  for (r = tr - 1, f = tf + 1; r >= 0 && f <= 7; r--, f++){
+    attacks |= (1ULL << (r * 8 + f));
+    if (1ULL << (r * 8 + f) & block) break;
+  }
+  for (r = tr + 1, f = tf - 1; r <= 7 && f >= 0; r++, f--){
+    attacks |= (1ULL << (r * 8 + f));
+    if (1ULL << (r * 8 + f) & block) break;
+  }
+    attacks |= (1ULL << (r * 8 + f));
+  for (r = tr - 1, f = tf - 1; r >= 0 && f >= 0; r--, f--){
+    attacks |= (1ULL << (r * 8 + f));
+    if (1ULL << (r * 8 + f) & block) break;
+  }
+  
+  return attacks;
+
+}
+
+U64 rook_attacks_on_the_fly(int square, U64 block)
+{
+
+  U64 attacks = 0ULL;
+  
+  int r, f;
+
+  int tr = square / 8;
+  int tf = square % 8;
+
+  for (r = tr + 1; r <= 7; r++) {
+    attacks |= (1ULL << (r * 8 + tf));
+    if (1ULL << (r * 8 + tf) & block) break;
+  }
+  for (r = tr - 1; r >= 0; r--) {
+    attacks |= (1ULL << (r * 8 + tf));
+    if (1ULL << (r * 8 + tf) & block) break;
+  }
+  for (f = tf + 1; f <= 7; f++) {
+    attacks |= (1ULL << (tr * 8 + f));
+    if (1ULL << (r * 8 + tf) & block) break;
+  }
+  for (f = tf - 1; f >= 0; f--) {
+    attacks |= (1ULL << (tr * 8 + f));
+    if (1ULL << (r * 8 + tf) & block) break;
+  }
+ 
+  return attacks;
+
+}
+
 void init_leapers_attacks()
 {
   // loop over 64 board squares
@@ -244,9 +316,16 @@ void init_leapers_attacks()
 int main()
 {
   init_leapers_attacks();
-  for (int square = 0; square < 64; square++) {
-    print_bitboard(mask_rook_attacks(square));
-  }
+  //for (int square = 0; square < 64; square++) {
+  //  print_bitboard(bishop_atacks_on_the_fly(square, 0ULL));
+  //}
+  U64 block = 0ULL;
+  set_bit(block, d7);
+  set_bit(block, d2);
+  set_bit(block, b4);
+  set_bit(block, g4);
+  print_bitboard(block);
 
+  print_bitboard(rook_attacks_on_the_fly(d4, block));
 	return 0;
 }
