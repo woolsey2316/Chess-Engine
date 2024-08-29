59f4275e5d17499dce909a7e9e4626274e1ef15c
David W <woolsey2316@gmail.com>
2024-08-29 20:45:12 +1000

    make move function

--- bbc.c
+++ bbc.c
@@ -1452,6 +1452,47 @@ void print_move_list(moves *move_list)
     memcpy(occupancies, occupancies_copy, 24);                            \
     side = side_copy, enpassant = enpassant_copy, castle = castle_copy;   \
 
+// move types
+enum { all_moves, only_captures };
+
+// make move on chess board
+static inline int make_move(int move, int move_flag)
+{
+    // quite moves
+    if (move_flag == all_moves)
+    {
+        // preserve board state
+        copy_board();
+        
+        // parse move
+        int source_square = get_move_source(move);
+        int target_square = get_move_target(move);
+        int piece = get_move_piece(move);
+        int promoted = get_move_promoted(move);
+        int capture = get_move_capture(move);
+        int double_push = get_move_double(move);
+        int enpass = get_move_enpassant(move);
+        int castling = get_move_castling(move);
+        
+        // move piece
+        pop_bit(bitboards[piece], source_square);
+        set_bit(bitboards[piece], target_square);
+    }
+    
+    // capture moves
+    else
+    {
+        // make sure move is the capture
+        if (get_move_capture(move))
+            make_move(move, all_moves);
+        
+        // otherwise the move is not a capture
+        else
+            // don't make it
+            return 0;
+    }
+}
+
 // generate all moves
 static inline void generate_moves(moves *move_list)
 {
@@ -1915,20 +1956,35 @@ int main()
     init_all();
     
     // parse fen
-    parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq c6 0 1 ");
+    parse_fen(tricky_position);
     print_board();
     
-    // preserve board state
-    copy_board();
+    // create move list instance
+    moves move_list[1];
     
-    // parse fen
-    parse_fen(empty_board);
-    print_board();
+    // generate moves
+    generate_moves(move_list);
     
-    // restore board state
-    take_back();
-
-    print_board();
+    // loop over generated moves
+    for (int move_count = 0; move_count < move_list->count; move_count++)
+    {
+        // init move
+        int move = move_list->moves[move_count];
+        
+        // preserve board state
+        copy_board();
+        
+        // make move
+        make_move(move, all_moves);
+        print_board();
+        getchar();
+        
+        // take back
+        take_back();
+        print_board();
+        getchar();
+    }
+    
     
     return 0;
 }
